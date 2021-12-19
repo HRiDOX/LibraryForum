85550e2bcb280073103b5d178aee9e6d536f3392
HRiDOX <86535180+HRiDOX@users.noreply.github.com>
2021-12-20 00:45:41 +0600

    UserList

---
+++ library-pager/client/src/components/UserList.jsx
@@ -0,0 +1,62 @@
+import React,{useEffect,useState} from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
+import { InviteIcon } from "../assets";
+
+
+const ListContainer = ({ children }) => {
+     return (
+      <div className="user-list__container" >
+          <div className="user-list__header">
+                <p>User</p>
+                <p>Invite</p>
+          </div>
+
+         {children}
+        </div>
+
+    )
+}
+
+const UserItem = () => {
+    return (
+        <div className="user-item__wrapper">
+            <div className="user-item__name-wrapper">
+               <Avatar />
+            </div>
+        </div>
+    )
+
+}
+
+const UserList = () => {
+    const { client } = useChatContext();
+   const [users,setUsers] = useState ([]);
+   const[loading, setLoading] = useState(false);
+   
+   useEffect(() => {
+        const getUsers = async () => {
+            if(loading) return;
+
+            setLoading(true);
+
+            try {
+                
+            } catch (error) {
+                
+            }
+
+        }
+   }, [filters])
+
+    return (
+        <div>
+            <ListContainer />
+            UserList
+            <ListContainer/>
+            
+        </div>
+
+    ) 
+}
+
+export default UserList;
